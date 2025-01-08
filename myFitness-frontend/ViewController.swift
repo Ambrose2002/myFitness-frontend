6fa5e6bbb3207c1b1a9062b78247a4b1a625309b
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-07 20:44:22 -0500

    Initial Commit

---
+++ myFitness-frontend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFitness-frontend
+//
+//  Created by Ambrose Blay on 1/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
